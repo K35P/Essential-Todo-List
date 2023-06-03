0762d7739e1750fe990b4125b3e7efb756fe47f7
Cristian Fasano <fasanoc1an@gmail.com>
2023-06-03 19:28:11 +0200

    Versione 1.1 - Aggiunto pomodoro timer

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Todo List
 const todoList = document.querySelector('#todo-list');
 const newTodoInput = document.querySelector('#new-todo');
 const addTodoButton = document.querySelector('#add-todo');
@@ -26,3 +27,54 @@ const deleteTodoButtons = document.querySelectorAll('.delete-todo');
 deleteTodoButtons.forEach(function(button) {
   button.addEventListener('click', deleteTodoItem);
 });
+
+// Pomodoro Timer
+
+const pomodoroDurationInput = document.querySelector('#pomodoro-duration');
+const breakDurationInput = document.querySelector('#break-duration');
+const timeDisplay = document.querySelector('#time-display');
+const startButton = document.querySelector('#start-button');
+const resetButton = document.querySelector('#reset-button');
+
+let countdown;
+let timerIsActive = false;
+
+function startTimer(duration, display) {
+  let timer = duration, minutes, seconds;
+  countdown = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      clearInterval(countdown);
+      display.textContent = "00:00";
+      timerIsActive = false;
+      new Audio('https://www.soundjay.com/misc/sounds/bell-ringing-05.mp3').play();
+      startTimer(breakDurationInput.value * 60, timeDisplay);
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(countdown);
+  timerIsActive = false;
+}
+
+function resetTimer() {
+  stopTimer();
+  timeDisplay.textContent = pomodoroDurationInput.value + ":00";
+}
+
+startButton.addEventListener('click', function() {
+  if (!timerIsActive) {
+    timerIsActive = true;
+    startTimer(pomodoroDurationInput.value * 60, timeDisplay);
+  }
+});
+
+resetButton.addEventListener('click', resetTimer);
